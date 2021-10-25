9562c8b27cd44694d9f13c5bf51fdfa00f95ce1a
AYogore <58088654+AYogore@users.noreply.github.com>
2021-10-25 12:13:03 -0500

    initialized event and vibesesh models

---
+++ src/app/models/VibeSession.ts
@@ -0,0 +1,24 @@
+export class VibeSession{
+
+    constructor(public name: string, public memberId: number, public memberName: string)
+    {
+        this.name = name;
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    getName()
+    {
+        return this.name;
+    }
+
+    getMemberId()
+    {
+        return this.memberId;
+    }
+
+    getMemberName()
+    {
+        return this.memberName;
+    }
+}
