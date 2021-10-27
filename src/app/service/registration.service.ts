b1e0d31dea10ce4d557bdf4cfbe81d82e55feeeb
jonathan.powers <jonathan.powers@revature.net>
2021-10-27 13:28:13 -0400

    Changed url inservice

--- src/app/service/registration.service.ts
+++ src/app/service/registration.service.ts
@@ -1,6 +1,7 @@
 import { newAccount } from '../models/newAccount';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -13,7 +14,7 @@ export class RegistrationService {
 
   constructor(private http: HttpClient) {}
 
-  url: string = 'http://localhost:8080/account/new';
+  url: string = environment.submitForm+"/account/new";
 
   newAccount(account: newAccount): Observable<Object> {
     console.log(account);
