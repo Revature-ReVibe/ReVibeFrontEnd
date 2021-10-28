e8dac6b16efa19f86dccc3ba0c62d4e1f8c2811b
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-10-28 10:40:06 -0700

    added reset password component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { GeneralFeedComponent } from "./components/posts/feeds/general-feed/general-feed.component";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { Routes } from "@angular/router";
@@ -7,20 +8,52 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./components/login/login.component";
 import { RegistrationComponent } from "./components/registration/registration.component";
+import { GeneralFormComponent } from "./general-form/general-form.component";
+import { UserProfileComponent } from "./components/user-profile/user-profile.component";
+import { EventsComponent } from "./components/events/events.component";
+import { VibeSessionComponent } from "./components/vibe-session/vibe-session.component";
+import { ResetPasswordComponent } from "./components/reset-password/reset-password.component";
 
 const routes: Routes = [
     {
-      path: "", 
+      path: "",
       component: HomeComponent
     },
     {
-      path: "login", 
+      path: "login",
       component: LoginComponent
+    },
+    
+    {
+      path:'users/:id',
+      component: UserProfileComponent
     },
 
     {
+        path: "feed",
+        component: GeneralFeedComponent
+    },
+    {
       path: "registration",
       component: RegistrationComponent
+    },
+    {
+      path: "form",
+      component: GeneralFormComponent
+    },
+    {
+      path: "events",
+      component: EventsComponent
+    },
+
+    {
+      path: "vibe-session",
+      component: VibeSessionComponent
+    },
+
+    {
+      path: "reset-password",
+      component: ResetPasswordComponent
     }
 ];
 
