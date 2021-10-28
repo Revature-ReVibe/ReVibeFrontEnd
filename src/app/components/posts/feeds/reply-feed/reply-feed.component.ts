4c5e4449461f578629a89e67a4a805cfe064eca6
Charles Bein <charles.bein@revature.net>
2021-10-28 16:19:32 -0400

    Made new component

---
+++ src/app/components/posts/feeds/reply-feed/reply-feed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reply-feed',
+  templateUrl: './reply-feed.component.html',
+  styleUrls: ['./reply-feed.component.css']
+})
+export class ReplyFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
