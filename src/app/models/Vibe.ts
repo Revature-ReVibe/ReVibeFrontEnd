e4a326864ac6ffc3edbc36660126c2f23961f3b0
TMSAVANG <thanath.savang@revature.net>
2021-10-29 11:17:53 -0400

    Adding like functionality (1st commit)

--- src/app/models/Vibe.ts
+++ src/app/models/Vibe.ts
@@ -1,5 +1,6 @@
 import { Account } from "./Account";
+import { Like } from "./Like";
 
 export class Vibe {
-  constructor(public vibeId: number, public poster: Account, public vibeMessage: string, public likes: Account[], public vibePic?: string, public parentVibe?: Vibe) {}
+  constructor(public vibeId: number, public poster: Account, public vibeMessage: string, public likes: Like[], public vibePic?: string, public parentVibe?: Vibe) {}
 }
