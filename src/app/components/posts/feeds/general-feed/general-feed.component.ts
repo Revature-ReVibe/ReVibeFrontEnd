a3d7719071fe2353cd7986af8048d1236d086a4e
Charles Bein <charles.bein@revature.net>
2021-11-01 17:58:17 -0400

    Endpoint connection working

--- src/app/components/posts/feeds/general-feed/general-feed.component.ts
+++ src/app/components/posts/feeds/general-feed/general-feed.component.ts
@@ -3,6 +3,7 @@ import {Vibe} from "../../../../models/Vibe";
 import {MatDialog} from "@angular/material/dialog";
 import {NewVibeComponent} from "../../new-vibe/new-vibe.component";
 import {Account} from "../../../../models/Account";
+import {VibeService} from "../../../../service/vibe.service";
 
 const postCount: number = 30;
 
@@ -15,15 +16,20 @@ export class GeneralFeedComponent implements OnInit {
 
   vibes: Vibe[] = [];
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog, private vibeService: VibeService) {
     //Dummy data, should be replaced with fetching data from server (get all posts)
-    for(let i = 0; i < postCount; i++) {
-      let newVibe: Vibe = new Vibe(i, "Message", 0, [], 1, "date", []);
-      this.vibes.push(newVibe);
-    }
+    // for(let i = 0; i < postCount; i++) {
+    //   let newVibe: Vibe = new Vibe(i, "Message", 0, [], 1, "date", []);
+    //   this.vibes.push(newVibe);
+    // }
   }
 
   ngOnInit(): void {
+    this.vibeService.getAllVibes().subscribe(
+      (data) => {
+        this.vibes = data;
+      }
+    )
   }
 
   openNewVibeDialog() {
---
+++ src/app/service/vibe.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {Vibe} from "../models/Vibe";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VibeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllVibes() {
+    return this.httpClient.get(environment.serverLocationURL + '/vibe/all/post') as Observable<Vibe[]>;
+  }
+
+  getVibeById(id: number) {
+    return this.httpClient.get(environment.serverLocationURL + '/vibe/find/' + id) as Observable<Vibe>;
+  }
+
+  createNewVibe(vibe: Vibe) {
+    return this.httpClient.post(environment.serverLocationURL + '/vibe/createVibe', JSON.stringify(vibe)) as Observable<Vibe>;
+  }
+
+  createNewReply(reply: Vibe, parentId: number) {
+    return this.httpClient.post(environment.serverLocationURL + '/vibe/createReply/' + parentId, JSON.stringify(reply)) as Observable<Vibe>;
+  }
+
+  getAllReplies(parentId: number) {
+    return this.httpClient.get(environment.serverLocationURL + '/vibe/allReplies/' + parentId) as Observable<Vibe[]>;
+  }
+
+  getVibesByCurrentUser() {
+    return this.httpClient.get(environment.serverLocationURL + '/find/account') as Observable<Vibe[]>;
+  }
+}
