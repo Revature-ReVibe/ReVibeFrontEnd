f1389f893917af35d0104da7275aaf9dedf1ecf9
AYogore <58088654+AYogore@users.noreply.github.com>
2021-11-01 09:48:33 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
   //set up localhost endpoints here
-  submitForm: "http://localhost:8088",
-  serverLocationURL: "http://localhost:8088"
+  submitForm: "http://54.221.18.214:8088/",
+  serverLocationURL: "http://54.221.18.214:8088/",
+  getUser : "http://54.221.18.214:8088/account/findbyId",
+  updateUser : "http://54.221.18.214:8088/account/updateprofile"
   
 };
