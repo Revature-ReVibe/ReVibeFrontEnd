0003e62b23f04b8fa301cb7a8a3c85e83e2bc7c0
AYogore <58088654+AYogore@users.noreply.github.com>
2021-11-02 20:28:49 -0500

    no more button

--- src/app/service/like.service.ts
+++ src/app/service/like.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Like } from '../models/Like';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,12 @@ export class LikeService {
 
   public getLikes(vibeId: number)
   {
-    return this.http.get<Like[]>('http://localhost:8088/vibe/likes/' + vibeId); 
+    return this.http.get<Like[]>(environment.serverLocationURL + '/vibe/likes/' + vibeId); 
   }
 
   public postLike(vibeId: number)
   {
-    return this.http.post<Like[]>('http://localhost:8088/vibe/like/' + vibeId, '');
+    return this.http.post<Like[]>(environment.serverLocationURL + '/vibe/like/' + vibeId, '');
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //set up localhost endpoints here
   submitForm: "http://54.221.18.214:8088/",
-  serverLocationURL: "http://54.221.18.214:8088/",
+  serverLocationURL: "http://54.221.18.214:8088",
   getUser : "http://54.221.18.214:8088/account/findbyId",
   updateUser : "http://54.221.18.214:8088/account/updateprofile"
   
